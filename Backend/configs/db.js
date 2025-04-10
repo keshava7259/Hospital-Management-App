92c8188f0813a182f874c809e8c5a137fcf5363b
Kastro Kiran V <122141717+KastroVKiran@users.noreply.github.com>
2025-04-10 17:06:28 +0530

    Update db.js

--- Backend/configs/db.js
+++ Backend/configs/db.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const connection = mongoose.connect(process.env.dbURL);
+const connection = mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 module.exports = { connection };
+
